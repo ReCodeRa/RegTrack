be9176441d8224ce886bb516c0ed287f47b4846a
ReCodeRa <esosacek@centrum.cz>
2022-11-14 11:44:22 +0100

    all page spc not working

--- sp_RegTrack/sp_RegTrack/spiders/sukl.py
+++ sp_RegTrack/sp_RegTrack/spiders/sukl.py
@@ -5,16 +5,19 @@ import scrapy
 class suklSpider(scrapy.Spider):
     name = "sukl"
 
-    start_urls = ["""https://www.sukl.cz/modules/medication/search.php?data%5Bsearch_for%5D=&data%5Bcode%5D=&data%5Batc_group%5D=&data%5Bmaterial%5D=loperamide&data%5Bpath%5D=&data%5Breg%5D=&data%5Bradio%5D=none&data%5Brc%5D=&data%5Bchbox%5D%5B%5D=braill-yes&data%5Bchbox%5D%5B%5D=braill-no&data%5Bchbox%5D%5B%5D=braill-def&data%5Bwith_adv%5D=0&search=Vyhledat&data%5Blisting%5D=100"""]
+    start_urls = ["""https://www.sukl.cz/modules/medication/search.php?data%5Bsearch_for%5D=&data%5Bcode%5D=&data%5Batc_group%5D=&data%5Bmaterial%5D=loperamide&data%5Bpath%5D=&data%5Breg%5D=&data%5Bradio%5D=none&data%5Brc%5D=&data%5Bchbox%5D%5B%5D=braill-yes&data%5Bchbox%5D%5B%5D=braill-no&data%5Bchbox%5D%5B%5D=braill-def&data%5Bwith_adv%5D=0&search=Vyhledat&data%5Blisting%5D=50"""]
     # URL cannot be parsed properly
         
     def parse(self, response): 
         for product in response.css('tr.first'):
             yield {
-                   'brand_pcg' : product.css('td  a::attr(title)').get() 
-                   'spc': product.css('td:nth-child(3) a::attr(href)').get()
-                   'detail': product.css('td a::attr(href)').get()
+                   'brand_pcg' : product.css('td  a::attr(title)').get(), 
+                   'spc': product.css('td[headers="spc"] a::attr(href)').get(), 
+                   'detail': product.css('td a::attr(href)').get(),
             } 
            
-           
-        next_page = response.css()
+        next_page = response.css('p.pager a.nav::attr(href)').get()  
+        if next_page is not None:
+           next_page_url = 'https://www.sukl.cz/' + next_page
+           yield response.follow(next_page_url, callback=self.parse)
+
